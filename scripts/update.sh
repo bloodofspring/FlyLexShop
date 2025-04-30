4daf01615bf0f9f51afe1117009a75df28c4304e
bloodofspring <t.shagin@gmail.com>
2025-04-30 18:02:29 +0300

    feat: Не знаешь почему permission denied? Используй sudo -- открывает все двери

--- scripts/update.sh
+++ scripts/update.sh
@@ -3,11 +3,12 @@
 set -e
 
 # Настраиваем Git для работы в CI/CD
-git config --global --add safe.directory "$(pwd)"
-git config --global core.autocrlf false
-git config --global core.fileMode false
+sudo git config --global --add safe.directory "$(pwd)"
+sudo git config --global core.autocrlf false
+sudo git config --global core.fileMode false
 
-git pull origin main
+# Выполняем git pull с sudo
+sudo git pull origin main
 if [ $? -ne 0 ]; then
   echo "Ошибка при выполнении git pull"
   exit 1
