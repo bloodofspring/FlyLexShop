ec99fd13d236c19de19a49a6dfffbf7215483986
bloodofspring <t.shagin@gmail.com>
2025-05-31 12:02:44 +0300

    feat: Added new configuration files

--- scripts/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Даем права на выполнение всем скриптам
-chmod +x scripts/*.sh
-
-# Запускаем скрипт настройки cron
-./scripts/setup-cron.sh
-
-# Запускаем контейнеры
-docker compose up -d
